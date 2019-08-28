0ffd7827063536a1020032188eac6bca59b904a4
Shirley Keter <shirleyketer@gmail.com>
2019-08-28 14:48:18 +0300

    data

--- data/models.py
+++ data/models.py
@@ -5,5 +5,6 @@ from django.db import models
 # Create your models here.
 class Date(models.Model):
     name = models.CharField(max_length=40)
-    value = models.CharField(max_length =60)
-    date = models.DateTimeField(auto_now_add=True)
+    value = models.FloatField()
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField(auto_now_add=True)
