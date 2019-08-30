c6e0836166014ba63b487d2a92dbc91bdbb18671
Shirley Keter <shirleyketer@gmail.com>
2019-08-30 17:08:15 +0300

    add

--- sqlite.py
+++ sqlite.py
@@ -10,15 +10,35 @@ cur = con.cursor()
 con.commit()
 r = requests.get('https://www.esrl.noaa.gov/psd/enso/mei/data/meiv2.data')
 # to_db = r.json()
-data = json.loads('{}')
-# url = requests.get('https://www.esrl.noaa.gov/psd/enso/mei')
-# content = requests.get(url).content
-# dataset = json.loads(content)
-# dataframe=list()
-des=data['start_date']['end_date']['value']
-code=data['start_date']['end_date']['value']
-cur.execute("INSERT INTO date (start_date,end_date ) VALUES (?, ?);",         
-(des,code))
-con.commit()
-con.close()
+d = json.loads('[]')
+dataframe = list()
+def data():
+  
+   date = datetime.datetime.strptime('%Y-%m-%d')
+   value = (str(float(row[1-11]),date))
+   cur.execute("INSERT INTO data (start_date,end_date ) value (?, ?);",) 
+   conn.commit()
+# print ("inserted") 
+
+
+# def display_data(con):
+try:
+    con = sqlite3.connect("data.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM date ")
+except Exception as E:
+    print ("Error: ", E)
+else:
+    for row in cur.fetchall():
+        print (row)
+con.close() 
+
+
+
+# des=data['']
+# code=data['start_date']['end_date']['value']
+# cur.execute("INSERT INTO date (start_date,end_date ) VALUES (?, ?);",         
+# (des,code))
+# con.commit()
+# con.close()
 
